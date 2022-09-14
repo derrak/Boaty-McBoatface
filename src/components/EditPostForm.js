523bff1ab7a6a76a8c11a07ee4477ddead0bf766
Derrak Richard <derrak@gmail.com>
2022-09-14 13:55:13 -0700

    add logic checks to input old post values if not updated

--- src/components/EditPostForm.js
+++ src/components/EditPostForm.js
@@ -5,16 +5,49 @@ import PropTypes from "prop-types";
 function EditPostForm (props) {
   const { post } = props;
 
+  // function handleEditPostFormSubmission(event) {
+  //   event.preventDefault();
+  //   props.onEditPost({
+  //     comment: event.target.comment.value, 
+  //     description: event.target.description.value, 
+  //     user: event.target.user.value, 
+  //     id: post.id,
+  //     voteCount: post.voteCount
+  //   });
+  // }
   function handleEditPostFormSubmission(event) {
     event.preventDefault();
+
+let editedComment = ""
+if (event.target.comment.value === "") {
+  editedComment = post.comment;
+} else {
+  editedComment = event.target.comment.value
+}
+
+let editedDescription = ""
+if (event.target.description.value === "") {
+  editedDescription = post.description;
+} else {
+  editedDescription = event.target.description.value
+}
+
+let editedUser = ""
+if (event.target.user.value === "") {
+  editedUser = post.user;
+} else {
+  editedUser = event.target.user.value
+}
+
     props.onEditPost({
-      comment: event.target.comment.value, 
-      description: event.target.description.value, 
-      user: event.target.user.value, 
+      comment: editedComment,
+      description: editedDescription, 
+      user: editedUser, 
       id: post.id,
-      voteCount: post.voteCount
+      voteCount: post.voteCount,
+      dateTime: post.dateTime
     });
-  }
+}
 
   return (
     <React.Fragment>
